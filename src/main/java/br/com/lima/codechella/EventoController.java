c3be6d868b9d6cd78d6b68a3b9d0be843166056e
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-23 19:39:09 -0300

    Commit Busca por ID

--- src/main/java/br/com/lima/codechella/EventoController.java
+++ src/main/java/br/com/lima/codechella/EventoController.java
@@ -3,9 +3,11 @@ package br.com.lima.codechella;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.awt.*;
 
@@ -16,9 +18,13 @@ public class EventoController {
     @Autowired
     private EventoService servico;
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping //(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<EventoDto> obterTodos() {
         return servico.obterTodos();
     }
 
+    @GetMapping("/{id}")
+    public Mono<EventoDto> obterPorId(@PathVariable Long id) {
+        return servico.obterPorId(id);
+    }
 }
--- src/main/java/br/com/lima/codechella/EventoService.java
+++ src/main/java/br/com/lima/codechella/EventoService.java
@@ -3,6 +3,7 @@ package br.com.lima.codechella;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EventoService {
@@ -12,6 +13,11 @@ public class EventoService {
 
     public Flux<EventoDto> obterTodos() {
         return repositorio.findAll()
+                .map(EventoDto::toDto);
+    }
+
+    public Mono<EventoDto> obterPorId(Long id) {
+        return repositorio.findById(id)
                 .map(EventoDto::toDto);
     }
 }
