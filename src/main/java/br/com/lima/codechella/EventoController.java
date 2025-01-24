ef2bdb32a695c8ddc564c86a1d40fc17f7b56441
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-24 16:02:26 -0300

    Commit Propagando as mudanças

--- src/main/java/br/com/lima/codechella/EventoController.java
+++ src/main/java/br/com/lima/codechella/EventoController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 import java.awt.*;
 
@@ -12,8 +13,14 @@ import java.awt.*;
 @RequestMapping("/eventos")
 public class EventoController {
 
-    @Autowired
-    private EventoService servico;
+
+    private  final EventoService servico;
+    private final Sinks.Many<EventoDto> eventosSink;
+
+    public EventoController(EventoService servico) {
+        this.servico = servico;
+        this.eventosSink = Sinks.many().multicast().onBackpressureBuffer();
+    }
 
     @GetMapping //(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<EventoDto> obterTodos() {
