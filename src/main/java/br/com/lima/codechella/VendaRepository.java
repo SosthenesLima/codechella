30b69c5f6b0ca85b582044a688afef7d167da4ae
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-25 13:21:21 -0300

    Commit controle de ingressos vendidos10

---
+++ src/main/java/br/com/lima/codechella/VendaRepository.java
@@ -0,0 +1,6 @@
+package br.com.lima.codechella;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface VendaRepository extends ReactiveCrudRepository<Venda, Long> {
+}
