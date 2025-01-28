5abdd731b7096d670521c1915d9656cc277747e7
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-28 10:51:13 -0300

    Commit Utilizando o WebClient02

--- src/main/java/br/com/lima/codechella/TraducaoDeTextos.java
+++ src/main/java/br/com/lima/codechella/TraducaoDeTextos.java
@@ -1,8 +1,28 @@
 package br.com.lima.codechella;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
 public class TraducaoDeTextos {
 
-    public static obterTraducao(String texto, String idioma) {
+    public static Mono<String> obterTraducao(String texto, String idioma) {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("https://api-free.deepl.com/v2/translate")
+                .build();
 
+        MultiValueMap<String, String> req = new LinkedMultiValueMap<>();
+
+        req.add("text", texto);
+        req.add("target_lang", idioma);
+
+        webClient.post()
+                .header("Authorization", "DeepL-Auth-Key " + System.getenv("DEEPL_APIKEY"))
+                .body(BodyInserters.fromFormData(req))
+                .retrieve()
+                .bodyToMono(Traducao.class)
+                 .map(Traducao::getTexto);
     }
 }
