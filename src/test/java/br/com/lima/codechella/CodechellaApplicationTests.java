75e280282540c4a8f5f3c261b2ac6ecee0f654c7
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-30 09:40:21 -0300

    Commit Implementando testes com WebTestClient01

--- src/test/java/br/com/lima/codechella/CodechellaApplicationTests.java
+++ src/test/java/br/com/lima/codechella/CodechellaApplicationTests.java
@@ -1,13 +1,20 @@
 package br.com.lima.codechella;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTestAotProcessor;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CodechellaApplicationTests {
 
+	@Autowired
+	private WebTestClient webClient;
+
 	@Test
-	void contextLoads() {
+	void cadastraNovoEvento() {
 	}
 
 }
