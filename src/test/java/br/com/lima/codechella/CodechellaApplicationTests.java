9211d7740effadc8ec4e117a0f8e0f9dc0d771a2
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-31 10:20:48 -0300

    Commit Implementando testes com WebTestClient02

--- src/test/java/br/com/lima/codechella/CodechellaApplicationTests.java
+++ src/test/java/br/com/lima/codechella/CodechellaApplicationTests.java
@@ -7,14 +7,32 @@ import org.springframework.boot.test.context.SpringBootTestAotProcessor;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CodechellaApplicationTests {
 
 	@Autowired
-	private WebTestClient webClient;
+	private WebTestClient webTestClient;
 
 	@Test
 	void cadastraNovoEvento() {
+		EventoDto dto = new EventoDto(null, TipoEvento.SHOW, "Kiss", LocalDate.parse("2025-02-19"), "Show da melhor banda que existe");
+
+		webTestClient.post().uri("/eventos").bodyValue(dto)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(EventoDto.class)
+				.value(response -> {
+					assertNotNull(response.id());
+					assertEquals(dto.tipo(), response.tipo());
+					assertEquals(dto.nome(), response.nome());
+					assertEquals(dto.data(), response.data());
+					assertEquals(dto.descricao(), response.descricao());
+				});
 	}
 
 }
